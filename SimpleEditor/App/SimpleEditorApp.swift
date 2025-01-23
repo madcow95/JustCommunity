a03949b007ad86f1765a6bcdddbf3c4b376d269e
MadCow <lobasketve@naver.com>
2025-01-23 19:44:07 +0900

    first commit

---
+++ SimpleEditor/App/SimpleEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleEditorApp.swift
+//  SimpleEditor
+//
+//  Created by MadCow on 2025/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
