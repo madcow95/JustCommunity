62032e722bcfd5fc0ba680ef44dfd0c099fe4d9f
MadCow <lobasketve@naver.com>
2025-01-30 00:57:00 +0900

    Design. 피드 화면 구성 - 1

--- SimpleEditor/Features/Home/HomeView.swift
+++ SimpleEditor/Features/Home/HomeView.swift
@@ -5,36 +5,97 @@ struct HomeView: View {
     @Bindable var store: StoreOf<HomeFeature>
     
     var body: some View {
-        NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
-            VStack {
-                Text("TEST")
-            }
-            .navigationTitle("HomeView")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button {
-                        store.send(.moveToAdd)
-                    } label: {
-                        Image(systemName: "plus")
+        GeometryReader { geometry in
+            NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
+                ScrollView {
+                    VStack {
+                        HStack {
+                            HStack {
+                                Image(systemName: "sun.max")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 40, height: 40)
+                                VStack(alignment: .leading) {
+                                    Text("닉네임 들어갈 곳")
+                                        .font(.subheadline)
+                                    Text("뭐 들어갈 거 있나?")
+                                        .font(.caption)
+                                }
+                            }
+                            Spacer()
+                            CustomButton(image: "ellipsis", color: .primaryColor) {
+                                
+                            }
+                        }
+                        .frame(width: geometry.size.width, height: 40)
+                        
+                        Image("testImg")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: geometry.size.width, height: 500)
+                        
+                        HStack {
+                            HStack {
+                                Button {
+                                    
+                                } label: {
+                                    Image(systemName: "heart")
+                                        .tint(.primaryColor)
+                                        .frame(width: 40, height: 40)
+                                }
+                                Button {
+                                    
+                                } label: {
+                                    Image(systemName: "message.fill")
+                                        .tint(.primaryColor)
+                                        .frame(width: 40, height: 40)
+                                }
+                                Button {
+                                    
+                                } label: {
+                                    Image(systemName: "square.and.arrow.up")
+                                        .tint(.primaryColor)
+                                        .frame(width: 40, height: 40)
+                                }
+                            }
+                            Spacer()
+                            HStack {
+                                CustomButton(image: "bookmark", color: .primaryColor) {
+                                    
+                                }
+                            }
+                        }
+                        .padding([.leading, .trailing], 10)
+                        .frame(width: geometry.size.width, height: 40)
                     }
-                    .tint(.primaryColor)
-                }
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button {
-                        store.send(.moveToSetting)
-                    } label: {
-                        Image(systemName: "gearshape.fill")
+                    .navigationTitle("HomeView")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar {
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Button {
+                                store.send(.moveToAdd)
+                            } label: {
+                                Image(systemName: "plus")
+                            }
+                            .tint(.primaryColor)
+                        }
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Button {
+                                store.send(.moveToSetting)
+                            } label: {
+                                Image(systemName: "gearshape.fill")
+                            }
+                            .tint(.primaryColor)
+                        }
                     }
-                    .tint(.primaryColor)
                 }
-            }
-        } destination: { store in
-            switch store.case {
-            case let .add(store):
-                AddView(store: store)
-            case let .setting(store):
-                SettingView(store: store)
+            } destination: { store in
+                switch store.case {
+                case let .add(store):
+                    AddView(store: store)
+                case let .setting(store):
+                    SettingView(store: store)
+                }
             }
         }
     }
