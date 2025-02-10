331620a0a621994135feeaf095287d6972f9dd9b
MadCow <lobasketve@naver.com>
2025-02-10 23:18:22 +0900

    Etc. Todo 작성 - 1

--- SimpleEditor/Features/Setting/SettingView.swift
+++ SimpleEditor/Features/Setting/SettingView.swift
@@ -12,6 +12,7 @@ struct SettingView: View {
     @Bindable var store: StoreOf<SettingFeature>
     
     var body: some View {
+        // MARK: TODO - 사용자의 설정에 대한 View를 그리기
         Text("Setting View!")
     }
 }
